1d62330eb9ec15e829d641c32626d6c334144282
Tom Paulus <tom@tompaulus.com>
2015-01-04 22:14:11 -0800

    Improved code (Added comments and added License Loader for StudentRND API - Which will be ignored by the user) Ignoring API.txt file which contains the API tokens for StudentRND or the user.

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,10 +2,50 @@
 # Written By Tom Paulus, @tompaulus, www.tompaulus.com
 
 from fancyhands import FancyhandsClient
+# You can install the FancyHands API using pip: "sudo pip install fancyhands"
 from datetime import datetime, timedelta
 
-api_key = ''
-secret = ''
+
+def read_api_keys(rel_file_path="./API.txt"):
+    """
+    Ignore this function, actually delete it before you run this file, all it does is try to open a file that contains
+    the StudentRND API Key and Secret.
+
+    :param rel_file_path: Location of the API Key File relative to the file being executed.
+    :return fh_key: FancyHands API Key
+    :return fh_secret: FancyHands API Secret
+    :rtype : tuple
+
+    """
+    fh_key = ''
+    fh_secret = ''
+
+    try:
+        with open(rel_file_path) as license_file:
+            line = license_file.readline()
+
+            while line != '':
+                if line.count('Key:') == 1:
+                    # Key Line
+                    fh_key = line[5: -1]
+
+                elif line.count('Secret:') == 1:
+                    # Secret Line
+                    fh_secret = line[8: -1]
+
+                line = license_file.readline()  # Advance to the next line in the file.
+
+    except IOError:
+        print 'The API Key file does not exist. Not Fatal.'
+        return None, None
+
+    return fh_key, fh_secret
+api_key, secret = read_api_keys()   # Delete this too.
+
+
+# Uncomment the lines below and insert your API Key and Secret that you got from FancyHands
+# api_key = ''
+# secret = ''
 
 client = FancyhandsClient(api_key, secret)
 """ Client is our interface to FancyHands Once we have initialized it, it will remember who we are thought the file """
@@ -16,6 +56,8 @@ def reddit(phone_number):
     Get the top story off of or Reddit and have phone_number be called telling them the title
     :param phone_number: The Phone Number of who should be called
     :type phone_number: str
+    :return: If it was successful
+    :rtype : bool
     """
 
     title = "Let me know what's new on the internet!"
@@ -26,5 +68,6 @@ def reddit(phone_number):
 
     client.standard_create(title, description % phone_number, bid, expiration_date)
     return True
+
 
 reddit("(111) 111-1111")
--- Response.py
+++ Response.py
@@ -4,8 +4,47 @@
 from fancyhands import FancyhandsClient
 from datetime import datetime, timedelta
 
-api_key = ''
-secret = ''
+
+def read_api_keys(rel_file_path="./API.txt"):
+    """
+    Ignore this function, actually delete it before you run this file, all it does is try to open a file that contains
+    the StudentRND API Key and Secret.
+
+    :param rel_file_path: Location of the API Key File relative to the file being executed.
+    :return fh_key: FancyHands API Key
+    :return fh_secret: FancyHands API Secret
+    :rtype : tuple
+
+    """
+    fh_key = ''
+    fh_secret = ''
+
+    try:
+        with open(rel_file_path) as license_file:
+            line = license_file.readline()
+
+            while line != '':
+                if line.count('Key:') == 1:
+                    # Key Line
+                    fh_key = line[5: -1]
+
+                elif line.count('Secret:') == 1:
+                    # Secret Line
+                    fh_secret = line[8: -1]
+
+                line = license_file.readline()  # Advance to the next line in the file.
+
+    except IOError:
+        print 'The API Key file does not exist. Not Fatal.'
+        return None, None
+
+    return fh_key, fh_secret
+api_key, secret = read_api_keys()   # Delete this too.
+
+
+# Uncomment the lines below and insert your API Key and Secret that you got from FancyHands
+# api_key = ''
+# secret = ''
 
 client = FancyhandsClient(api_key, secret)
 
@@ -16,6 +55,8 @@ def call_and_react(phone_number):
 
     :param phone_number: The Phone Number of who should be called
     :type phone_number: str
+    :return:If it was successful
+    :rtype: bool
     """
 
     title = "Prank call my friend!"
@@ -28,7 +69,8 @@ def call_and_react(phone_number):
         {
             'label': 'Response',
             'type': 'textarea',
-            'description': 'Their response to the prank.',
+            'description': 'Their response to the prank. Please be as descriptive as possible. '
+                           'Do not say "Competed call as requested. Thanks!',
             'order': 1,
             "required": True
         }
@@ -38,4 +80,5 @@ def call_and_react(phone_number):
     return True
 
 
+print("Make Sure that the WebHooks URL on the Dashboard is up-to-date")
 call_and_react("(555) 555-555")
